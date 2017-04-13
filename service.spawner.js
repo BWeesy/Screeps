6ac336986771aeffcd59ea82881b4df7ff438d23
BWeesy <Benedictwesson@gmail.com>
2017-04-13 04:29:38 +0100

    Spawner bugfix
    Misnamed new upgraders and builders

--- service.spawner.js
+++ service.spawner.js
@@ -20,13 +20,13 @@ var spawner = {
         }
         
         if(upgraders.length < targetUpgraders) {
-            updaterName = 'harvester' + upgraders.length.toString();
+            updaterName = 'updater' + upgraders.length.toString();
             var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], updaterName, {role: 'upgrader'});
             console.log('Spawning new upgrader: ' + newName);
         }
 
         if(builders.length < targetBuilders) {
-            builderName = 'harvester' + builders.length.toString();
+            builderName = 'builder' + builders.length.toString();
             var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], builderName, {role: 'builder'});
             console.log('Spawning new builder: ' + newName);
         }
