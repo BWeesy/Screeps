d4e761ee450b84309d63dd7b85220469ecddd882
BWeesy <Benedictwesson@gmail.com>
2017-04-13 16:16:37 +0100

    Storage added to targetting service
    Targets closest non-full storage container. If none found, defaults to spawner logic

--- service.targetting.js
+++ service.targetting.js
@@ -19,6 +19,31 @@ var targetter = {
         return targetSpawner
 
     }
+
+    storage: function(creep) {
+        var targets = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_STORAGE) &&
+                    structure.energy < structure.energyCapacity;
+            }
+        });
+
+        if (targets) {
+            targetStorage = _.sortBy(targets, t => creep.pos.getRangeTo(t))[0];
+            return targetStorage
+        } else{
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                        structure.energy < structure.energyCapacity;
+                }
+            });
+            targetSpawn = _.sortBy(targets, t => creep.pos.getRangeTo(t))[0];
+            return targetSpawn
+        }
+        
+
+    }
 };
 
 module.exports = targetter;
