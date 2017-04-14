45ebfed0ef8a4371fff721fc3728f03da5526a70
BWeesy <Benedictwesson@gmail.com>
2017-04-14 14:58:06 +0100

    Builder hotfix
    Had an extra bracket int he wrong place, left some targetting undefined in some cases.

--- role.builder.js
+++ role.builder.js
@@ -21,21 +21,21 @@ var roleBuilder = {
 	    		if(creep.repair(targetRoad) == ERR_NOT_IN_RANGE) {
                 	creep.moveTo(targetRoad, {visualizePathStyle: {stroke: '#ffaa00'}});
             	}
-	    	}
-	    } else if (targetBuild) {
+	    	} else if (targetBuild) {
 	    		if(creep.build(targetBuild) == ERR_NOT_IN_RANGE) {
                 	creep.moveTo(targetBuild, {visualizePathStyle: {stroke: '#ffaa00'}});
             	}
-	    } else if (targetWall){
+	    	} else if (targetWall){
 	    		if(creep.repair(targetWall) == ERR_NOT_IN_RANGE) {
                 	creep.moveTo(targetWall, {visualizePathStyle: {stroke: '#ffaa00'}});
             	}
-	    } else {
-	        targetStorage = targetter.withdraw(creep);
-            if(creep.harvest(targetStorage) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targetStorage, {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-	    }
+	    	} else {
+	        	targetStorage = targetter.withdraw(creep);
+            	if(creep.harvest(targetStorage) == ERR_NOT_IN_RANGE) {
+             	   creep.moveTo(targetStorage, {visualizePathStyle: {stroke: '#ffaa00'}});
+            	}
+	    	}
+		}
 	}
 };
 
